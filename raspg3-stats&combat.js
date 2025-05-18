b248c75a314f59d7342965c956f6ab233b5792e0
Rasutei <rasutei@outlook.com>
2025-05-18 14:35:20 -0300

    changed: moved Statful into new "stats&combat" file

---
+++ raspg3-stats&combat.js
@@ -0,0 +1,221 @@
+/** @typedef {{calculation: (stat: Stat) => number, roundToNearest: number | false }} StatTypeOptions  */
+
+//# Module
+
+//# Classes
+class StatType {
+	static #all = new Map()
+	static #sets = new Map()
+	static precision = 6
+	name
+	calculation
+	roundToNearest = .01
+	global = []
+
+	/**
+	 * @param {String} name
+	 * @param {StatTypeOptions} options
+	 */
+	constructor(name, options) {
+		HookModule.run('before:StatType.constructor', arguments, this)
+
+		this.name = name
+		this.calculation = options.calculation || ((stat) => stat.base)
+		this.roundToNearest = options.roundToNearest || false
+
+		StatType.#all.set(name, this)
+
+		HookModule.run('after:StatType.constructor', arguments, this)
+	}
+
+	static get sets() {
+		return new Set(this.#sets)
+	}
+	static get all() {
+		return new Set(this.#all)
+	}
+
+	/** Returns the stat type with the given name, if found, or `null`, if not found.
+	 * @param {string} type Convention: no spaces, camelCase. Can be organized into domains (i.e. 'item.drop').
+	 */
+	static find(type) {
+		HookModule.run('StatType.find', arguments, this)
+		return this.#all.get(type) || null
+	}
+	/** Attempts to resolve a string to a StatType instance. Passes it back if first parameter is already one.
+	 * @param {string | StatType} type
+	 */
+	static resolve(type) {
+		HookModule.run('before:StatType.resolve', arguments, this)
+
+		if (StatType.isPrototypeOf(type))
+			return type
+		if (typeof(type) === 'string')
+			return this.find(type)
+
+		HookModule.run('after:StatType.resolve', arguments, this)
+	}
+	/** Creates and registers a set of stat types. Returns `true`, if successful, and `false`, if an error occurs.
+	 * @param {string} name
+	 * @param {Array<StatType> | Array<string>} stats
+	 */
+	static createSet(name, stats) {
+		HookModule.run('before:StatType.createSet', arguments, this)
+
+		const set = new Set()
+		for (const stat of stats)
+			set.add(StatType.resolve(stat).name)
+		this.#sets.set(name, set)
+
+		HookModule.run('after:StatType.createSet', arguments, this)
+		return true
+	}
+	/** Applies a set of StatTypes to the object. Returns `true`, if successful, and `false`, if an error occurs.
+	 * @param {string} set
+	 * @param {GameObject} object
+	 */
+	static applySetTo(set, object) {
+		HookModule.run('before:StatType.applySetTo', arguments, this)
+
+		if (!this.#sets.has(set))
+			return LOGS.elementNotRegisteredInCollection(set, 'StatType.#sets')
+
+		HookModule.run('after:StatType.applySetTo', arguments, this)
+	}
+
+	/** Calculates a given stat's final value.
+	 * @param {Stat} stat
+	 */
+	calculate(stat) {
+		HookModule.run('before:StatType.instance.calculate', arguments, this)
+
+		let netValue = +(this.calculation(stat)).toPrecision(6)
+		if (this.roundToNearest)
+			netValue -= netValue % this.roundToNearest
+
+		HookModule.run('after:StatType.instance.calculate', arguments, this)
+		return netValue
+	}
+	/** Creates and returns a Stat instance with this StatType, with the given initial value.
+	 * @param {number} initialValue
+	 */
+	create(initialValue) {
+		HookModule.run('StatType.instance.create', arguments, this)
+		return new Stat(this, initialValue)
+	}
+}
+class Stat {
+	#type
+	base
+	modifiers = []
+
+	/**
+	 * @param {StatType | string} type
+	 * @param {number} initialValue
+	 */
+	constructor(type, initialValue) {
+		HookModule.run('before:Stat.constructor', arguments, this)
+
+		if (typeof(type) === 'string')
+			this.#type = type
+		else if (type instanceof StatType)
+			this.#type = type.name
+		this.base = initialValue
+
+		HookModule.run('after:Stat.constructor', arguments, this)
+	}
+
+	/** @return {StatType} */
+	get type() {
+		return StatType.resolve(this.#type)
+	}
+
+	calculate() {
+		HookModule.run('Stat.instance.calculate', arguments, this)
+		return this.type.calculate(this)
+	}
+}
+
+//# Components
+class Statful {
+	static reference = '_stats'
+	static #stats = new Map()
+	#stats = new Map()
+	//? Note: Expected to be bundled with the Combat module.
+
+	static create(stat, options) {}
+
+	/** Gets the value correlated with the given stat name.
+	 * @param {string} stat Convention: no spaces, camelCase.
+	 */
+	get(stat) {
+		HookModule.run('Combatable.instance.get', arguments, this)
+		return this.#stats[stat]
+	}
+	/** Sets the value correlated with the given stat name. Returns `true`, if successful, and `false`, if the stat does not exist.
+	 * @param {string} stat Convention: no spaces, camelCase.
+	 * @param {boolean | number} value
+	 */
+	set(stat, value) {
+		HookModule.run('before:Combatable.instance.set', arguments, this)
+		if (typeof(stat) !== 'string')
+			throw EXCEPTIONS.brokenEnforcedType('Combatable.instance.set.stat', 'string')
+		switch (typeof(value)) {
+			case 'number':
+			case 'boolean':
+				break
+			default:
+				throw EXCEPTIONS.brokenEnforcedType('Combatable.instance.set.value', 'number | boolean')
+		}
+		if (!this.#stats.hasOwnProperty(stat)) {
+			LOGS.elementNotRegisteredInCollection(stat, 'Combatable.instance.#stats')
+			return false
+		}
+		const previous = this.#stats[stat]
+		this.#stats[stat] = value
+		EventModule.emitPropertyEvents({
+			object: this.parent,
+			property: stat,
+			previous,
+			current: value
+		}, 'stats.')
+		HookModule.run('after:Combatable.instance.set', arguments, this)
+		return true
+	}
+	/** Adds a stat and a default value. Returns `true`, if successful, and `false`, if the stat already exists, ignoring the operation.
+	 * @param {string} stat Convention: no spaces, camelCase.
+	 * @param {boolean | number} value
+	 */
+	create(stat, initialValue) {
+		HookModule.run('before:Combatable.instance.create', arguments, this)
+		if (typeof(stat) !== 'string')
+			throw EXCEPTIONS.brokenEnforcedType('Combatable.instance.create.stat', 'string')
+		switch (typeof(initialValue)) {
+			case 'number':
+			case 'boolean':
+				break
+			default:
+				throw EXCEPTIONS.brokenEnforcedType('Combatable.instance.set.initialValue', 'number | boolean')
+		}
+		if (this.#stats[stat] === undefined)
+			return false
+		this.#stats[stat] = initialValue
+		EventModule.emit(`stats.created`, {
+			object: this.parent,
+			stat,
+			initialValue
+		})
+		HookModule.run('after:Combatable.instance.create', arguments, this)
+		return true
+	}
+	/** Modifies an existing stat by a delta. Returns `true`, if successful, and `false`, if the stat does not exist.
+	 * @param {string} name
+	 * @param {number} delta
+	 */
+	modifyStat(name, delta) {}
+}
+class Combatant {
+	static reference = '_combat'
+	static requires = [Actionable, Agentive, Statful]
+	//? Note: Expected to be bundled with the Combat module.
+}
